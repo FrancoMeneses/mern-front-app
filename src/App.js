96402c45ffb95ffd2d3c393f7dbea8e093baf37b
Franco Meneses <meneses773@gmail.com>
2023-01-05 14:37:32 -0600

    Add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import NewForm from './pages/NewForm'
+import Temas from './pages/Temas'
+import UserHomePage from './pages/UserHomePage'
+import UserSettings from './pages/UserSettings'
+import NotFound from './pages/NotFound'
+import { PostProvider } from './context/PostContainer'
+
+const Layout = () => {
+  return(
+    <>
+    <Navbar></Navbar>
+    <div className='bg-red-500'>
+    <Outlet></Outlet>
+    </div>
+    <Footer></Footer>
+    </>
+  )
+}
+
+const router = createBrowserRouter([{
+  element: <Layout />,
+  children: [
+    {
+      path: "/",
+      element: <HomePage />
+    },
+    {
+      path:"/temas",
+      element: <Temas />
+    },
+    {
+      path: "/new",
+      element: <NewForm />
+    },
+    {
+      path: "/posts/:id",
+      element: <NewForm />
+    },
+    {
+      path: "/userhome",
+      element: <UserHomePage/>
+    },
+    {
+      path: "/usersettings",
+      element: <UserSettings />
+    },
+    {
+      path: "/*",
+      element: <NotFound/>
+    }
+  ]
+}])
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <PostProvider>
+    <RouterProvider router={router} />
+    </PostProvider>
+  )
 }
 
 export default App;
